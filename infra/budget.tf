3976d06a8b81a39e7d5274d48917ee2343989724
Daniel Ibarrola <dis96051@gmail.com>
2026-07-31 13:21:39 -0600

    fix: daily budget doesn't support forecast

--- infra/budget.tf
+++ infra/budget.tf
@@ -13,13 +13,4 @@ resource "aws_budgets_budget" "daily_cost_budget" {
     notification_type          = "ACTUAL"
     subscriber_email_addresses = ["daniel.ibarrola.sanchez@gmail.com"]
   }
-
-  # Alarm 2: Sends email if FORECASTED costs exceed 100% of budget ($100)
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = ["daniel.ibarrola.sanchez@gmail.com"]
-  }
 }
